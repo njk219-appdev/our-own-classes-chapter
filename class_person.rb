4c9174afe0c5a3300001b5a726688098eb566716
Noah Katz <noahkatz@chicagobooth.edu>
2023-04-05 17:17:30 +0000

    Complete

--- class_person.rb
+++ class_person.rb
@@ -6,9 +6,25 @@
 #  - An instance method, full_name, that puts first_name and last_name together
 #  - An instance method, age, that calculates the number of years between today and birthdate
 
+require("date")
+
 class Person
-  
+  attr_accessor :first_name
+  attr_accessor :last_name
+  attr_accessor :birthdate
+  attr_accessor :age
+  def full_name
+    return self.first_name + " " + self.last_name
+  end
+  def age
+    dob = Date.parse(self.birthdate)
+    now = Date.today
+    age_in_days = now - dob
+    age_in_years = age_in_days / 365
+    return age_in_years.to_i
+  end
 end
+
 
 # Test your instance methods by adding to the end of this file:
 
